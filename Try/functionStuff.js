c373a78fa45252f8b72879203cfd764eca4232ab
Jennisaurus <34190478+Jennisaurus@users.noreply.github.com>
2018-03-14 12:42:10 -0400

    added the comment summary table thing too!
    again, this is a super hacky way I put things together so....here's hopin it works :')

--- Try/functionStuff.js
+++ Try/functionStuff.js
@@ -2,7 +2,7 @@
 
 function attachHandlers(){
     /*trial();*/
-    displayTopEnt();
+    /*displayTopEnt();*/
     /*colourChg();*/
     
 
@@ -19,12 +19,22 @@ function trial(){
 
 window.onload = function(){
     var items = {"Entity 1": 0.5, "Entity 2": 1, "Entity 3": 0};
-    var endList = "<ol>";
+    var finalList = "<ol>";
     for(var index in items) {
-        endList += "<li>" +index + " (" + items[index] + ")</li>";
+        finalList += "<li>" +index + " (" + items[index] + ")</li>";
         }
-    endList += "</ol>";
-    document.getElementById("displayTable").innerHTML = endList;
+    finalList += "</ol>";
+    document.getElementById("displayList").innerHTML = finalList;
+    
+    
+    var comments = {"Comment 1": 88, "Comment 2": 50, "Comment 3": 20};
+    var finalTable = "<table> <thead><tr><th>Representative Comment</th><th>% of Comments</th></tr></thead> <tbody>";
+    for(var index2 in comments) {
+        finalTable += "<tr> <td>" +index2 + "</td> <td>" + comments[index2] + "%</td></tr>";
+        }
+    finalTable += "</tbody></table>";
+    document.getElementById("displayTable").innerHTML = finalTable;
+    
 }
 
 function colourChg(){
