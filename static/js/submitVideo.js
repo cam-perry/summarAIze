0ec8d93fa813ae71a83a326ada0581b4336e123b
cam-perry <33005811+cam-perry@users.noreply.github.com>
2018-04-04 14:59:53 -0400

    Enforce 500 comment/video maximum (for post-class production)

--- static/js/submitVideo.js
+++ static/js/submitVideo.js
@@ -1,3 +1,5 @@
+import adminEmail from '../config.js';
+
 // makes it so that you can press enter to submit the channel button
 $(document).ready(function() {
   $(window).keydown(function(event){
@@ -37,41 +39,53 @@ $('#cards-container').on('click', '.video-selector', (evt) => {
           var VIDEO = response.results.items[0];
           const total_comments = response.results.items[0].statistics.commentCount;
 
-          // display success message and create progress bar
-          $('#alert-box').html('')
-          $('#alert-box').html(
-            '<div class="alert alert-success">' +
-            '<strong>Success!</strong> Fetching and analyzing ' + total_comments + ' comments.' +
-            '</div>'
-          );
-          $('#progress-box').html('')
-          $('#progress-box').html(
-            '<div class="progress">' +
-              '<div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="'+ total_comments +'" style="width: 0%"></div>' +
-            '</div>'
-          );
+          if (total_comments > 500) {
+              // prevent over 500 comments from being analyzed
+              $('.video-selector').removeClass('disabled');
+              $('#channel-submit-btn').removeClass('disabled');
+              $('#alert-box').html('')
+              $('#alert-box').html(
+                '<div class="alert alert-warning">' +
+                  'Looks like you have a lot of comments. Contact ' + adminEmail + ' to request Premium access and analyze over 500 comments.' +
+                '</div>'
+              );
+          } else {
+            // display success message and create progress bar
+            $('#alert-box').html('')
+            $('#alert-box').html(
+              '<div class="alert alert-success">' +
+              '<strong>Success!</strong> Fetching and analyzing ' + total_comments + ' comments.' +
+              '</div>'
+            );
+            $('#progress-box').html('')
+            $('#progress-box').html(
+              '<div class="progress">' +
+                '<div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="'+ total_comments +'" style="width: 0%"></div>' +
+              '</div>'
+            );
 
-          // after confirming video, start the comment processing
-          $.ajax({
-             url: '/api/comments?videoId=' + videoId,
-             method: 'GET',
-             beforeSend: function() {
-               $('#YTurl').prop('disabled', true);
-             },
-             success: function(response) {
-               waitForWatsonUploads(response.results.environment_id, response.results.collection_id, total_comments, videoId);
-             },
-             error: function() {
-               $('#alert-box').html('')
-               $('#alert-box').html(
-                 '<div class="alert alert-danger">' +
-                 '<strong>Error!</strong> We are having trouble getting comments for that video URL.' +
-                 '</div>'
-               )
-             }
-          });
-        }
-      })
+            // after confirming video, start the comment processing
+            $.ajax({
+               url: '/api/comments?videoId=' + videoId,
+               method: 'GET',
+               beforeSend: function() {
+                 $('#YTurl').prop('disabled', true);
+               },
+               success: function(response) {
+                 waitForWatsonUploads(response.results.environment_id, response.results.collection_id, total_comments, videoId);
+               },
+               error: function() {
+                 $('#alert-box').html('')
+                 $('#alert-box').html(
+                   '<div class="alert alert-danger">' +
+                   '<strong>Error!</strong> We are having trouble getting comments for that video URL.' +
+                   '</div>'
+                 )
+               }
+            }) // end second AJAX
+          } // end else
+        } // end first AJAX success
+      }) // end first AJAX
 
     return false;
 });
